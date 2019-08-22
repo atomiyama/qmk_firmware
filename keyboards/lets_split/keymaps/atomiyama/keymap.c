85d1ffd4adc1ef736c3858933f7c42d1a68b9b5d
atomiyama <tomisuker16@gmail.com>
2019-08-22 16:40:06 +0900

    update

--- keyboards/lets_split/keymaps/atomiyama/keymap.c
+++ keyboards/lets_split/keymaps/atomiyama/keymap.c
@@ -20,6 +20,7 @@ enum custom_keycodes {
 #define LOWER  MO(_LOWER)
 #define RAISE  MO(_RAISE)
 #define ADJUST MO(_ADJUST)
+#define TAPPING_TERM 100
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Qwerty
@@ -36,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT_ortho_4x12(
    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
    KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS, \
+   SFT_T(KC_SPC), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS, \
    KC_LCTL, ADJUST,  KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_ENT,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT \
 ),
 
@@ -44,36 +45,36 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * |  `   |  1   |  2   |  3   |  4   |  5   |  6   |  7   |  8   |  9   |  0   |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |  4   |  5   |  6   |  *   |      |
+ * |      |  ~   |  !   |  @   |  #   |  $   |      |  4   |  5   |  6   |  -   |  _   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |  1   |  2   |  3   |  /   |      |
+ * |      |      |  %   |  ^   |  &   |  *   |      |  1   |  2   |  3   |  /   |  =   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |      |      |      |      |      |Enter |      |  0   |  +   |  -   |      |
+ * |      |      |      |      |      |      |Enter |  +   |  0   |  -   |  *   |  &   |
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_ortho_4x12( \
    KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,     KC_9,    KC_0, _______, \
-  _______, _______, _______, _______, _______, _______, _______,    KC_4,    KC_5,     KC_6, KC_ASTR, _______, \
-  _______, _______, _______, _______, _______, _______, _______,    KC_1,    KC_2,     KC_3, KC_SLSH, _______, \
-  _______, _______, _______, _______, _______, _______,  KC_ENT, _______,    KC_0,  KC_PLUS, KC_MINS, _______ \
+  _______, KC_TILD, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, _______,    KC_4,    KC_5,     KC_6, KC_MINS, KC_UNDS, \
+  _______, _______, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, _______,    KC_1,    KC_2,     KC_3, KC_SLSH,  KC_EQL, \
+  _______, _______, _______, _______, _______, _______,  KC_ENT, KC_PLUS,    KC_0,  KC_MINS, KC_ASTR, KC_AMPR \
 ),
 
 /* Raise
  * ,-----------------------------------------------------------------------------------.
- * |  `   |  (   |  )   |  !   |  @   |  #   |      |  7   |  8   |  9   |  0   |      |
+ * |  `   |  1   |  2   |  3   |  4   |  5   |  6   |  7   |  8   |  9   |  0   |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |  _   |  [   |  ]   |  $   |  %   |  ^   |      |  4   |  5   |  6   |  *   |      |
+ * |      |      |  {   |  [   |  (   |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |  {   |  }   |  &   |  *   |  -   |      |  1   |  2   |  3   |  /   |      |
+ * |      |      |  }   |  ]   |  )   |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |      |      | GUI  |      |      |Enter |      |  0   |  +   |  -   |      |
+ * |      |      |      | GUI  |      |      |Enter |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_ortho_4x12( \
-   KC_GRV, KC_LPRN, KC_RPRN, KC_EXLM,   KC_AT, KC_HASH, _______,    KC_7,    KC_8,    KC_9,    KC_0, _______, \
-  KC_UNDS, KC_LBRC, KC_RBRC,  KC_DLR, KC_PERC, KC_CIRC, _______,    KC_4,    KC_5,    KC_6, KC_ASTR, _______, \
-  _______, KC_LCBR, KC_RCBR, KC_AMPR, KC_ASTR, KC_MINS, _______,    KC_1,    KC_2,    KC_3, KC_SLSH, _______, \
-  _______, _______, _______, KC_RGUI, _______, _______,  KC_ENT, _______,    KC_0, KC_PLUS, KC_MINS, _______ \
+   KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, _______, \
+  _______, _______, KC_LCBR, KC_LBRC, KC_LPRN, _______, _______, _______, _______, _______, _______, _______, \
+  _______, _______, KC_RCBR, KC_RBRC, KC_RPRN, _______, _______, _______, _______, _______, _______, _______, \
+  _______, _______, _______, KC_RGUI, _______, _______,  KC_ENT, _______, _______, _______, _______, _______ \
 ),
 
 /* Adjust (Lower + Raise)
@@ -135,6 +136,11 @@ uint32_t layer_state_set_user(uint32_t state) {
   return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
 }
 
+static bool lower_pressed = false;
+static uint16_t lower_pressed_time = 0;
+static bool raise_pressed = false;
+static uint16_t raise_pressed_time = 0;
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
     case QWERTY:
@@ -152,6 +158,48 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         set_single_persistent_default_layer(_DVORAK);
       }
       return false;
+        case LOWER:
+      if (record->event.pressed) {
+        lower_pressed = true;
+        lower_pressed_time = record->event.time;
+
+        layer_on(_LOWER);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      } else {
+        layer_off(_LOWER);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+
+        if (lower_pressed && (TIMER_DIFF_16(record->event.time, lower_pressed_time) < TAPPING_TERM)) {
+          register_code(KC_LANG2); // for macOS
+          register_code(KC_MHEN);
+          unregister_code(KC_MHEN);
+          unregister_code(KC_LANG2);
+        }
+        lower_pressed = false;
+      }
+      return false;
+      break;
+    case RAISE:
+      if (record->event.pressed) {
+        raise_pressed = true;
+        raise_pressed_time = record->event.time;
+
+        layer_on(_RAISE);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      } else {
+        layer_off(_RAISE);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+
+        if (raise_pressed && (TIMER_DIFF_16(record->event.time, raise_pressed_time) < TAPPING_TERM)) {
+          register_code(KC_LANG1); // for macOS
+          register_code(KC_HENK);
+          unregister_code(KC_HENK);
+          unregister_code(KC_LANG1);
+        }
+        raise_pressed = false;
+      }
+      return false;
+      break;
   }
   return true;
 }
